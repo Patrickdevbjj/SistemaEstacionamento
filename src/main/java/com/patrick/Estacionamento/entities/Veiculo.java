67f7befba5174d2238bf17fabb3b9c3a8405252e
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-04-01 22:26:26 -0300

    Vehicle update

--- src/main/java/com/patrick/Estacionamento/entities/Veiculo.java
+++ src/main/java/com/patrick/Estacionamento/entities/Veiculo.java
@@ -1,21 +1,31 @@
 package com.patrick.Estacionamento.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
-
-@Data
+@Getter
+@Setter
 @Entity
 public class Veiculo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String placa;
     private String modelo;
     private LocalDateTime entrada;
     private LocalDateTime saida;
+
+    public Veiculo(String placa, String modelo) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.entrada = LocalDateTime.now(); // Registra a entrada automaticamente no momento da criação do veículo
+    }
 
 }
