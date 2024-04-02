26f44cd138f2fed8dcae79e007c13ffcb5833f82
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-04-01 22:43:37 -0300

    Create RegistrationEntry entity

---
+++ src/main/java/com/patrick/Estacionamento/entities/RegistroEntrada.java
@@ -0,0 +1,29 @@
+package com.patrick.Estacionamento.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+public class RegistroEntrada {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Veiculo veiculo;
+
+    private LocalDateTime dataHoraEntrada;
+
+    public RegistroEntrada(Veiculo veiculo, LocalDateTime dataHoraEntrada) {
+        this.veiculo = veiculo;
+        this.dataHoraEntrada = dataHoraEntrada;
+    }
+
+
+}
