f82bf134a977cf1a641e4a8d803d3c94320cf02a
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-04-08 17:02:46 -0300

    create method for find car by plate

--- src/main/java/com/patrick/Estacionamento/repositories/VeiculoRepository.java
+++ src/main/java/com/patrick/Estacionamento/repositories/VeiculoRepository.java
@@ -7,5 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
 
-}
+        Veiculo findByPlate(String placa);
+    }
+
+
 
