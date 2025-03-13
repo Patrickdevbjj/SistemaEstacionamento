79ec8b77bb795542f5ecad02eee092454f3e7760
Patrick Rodrigues <patrickdev6@hotmail.com>
2025-03-13 13:42:52 -0300

    Changes in VeiculoController and repository

--- src/main/java/com/patrick/Estacionamento/controllers/VeiculoController.java
+++ src/main/java/com/patrick/Estacionamento/controllers/VeiculoController.java
@@ -1,5 +1,49 @@
 package com.patrick.Estacionamento.controllers;
 
+import com.patrick.Estacionamento.entities.RegistroEntrada;
+import com.patrick.Estacionamento.entities.Veiculo;
+import com.patrick.Estacionamento.repositories.RegistroEntradaRepository;
+import com.patrick.Estacionamento.repositories.VeiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/veiculos")
 public class VeiculoController {
 
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+
+    @Autowired
+    private RegistroEntradaRepository registroEntradaRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Map<String, Object>>> listarVeiculos() {
+        List<Veiculo> veiculos = veiculoRepository.findAll();
+
+        List<Map<String, Object>> resultado = veiculos.stream().map(veiculo -> {
+            Map<String, Object> dados = new HashMap<>();
+            dados.put("placa", veiculo.getPlaca());
+
+
+            // Buscar a última entrada do veículo
+            RegistroEntrada ultimaEntrada = registroEntradaRepository
+                    .findTopByVeiculoOrderByDataHoraEntradaDesc(veiculo);
+
+            dados.put("ultimaEntrada",
+                    ultimaEntrada != null ? ultimaEntrada.getDataHoraEntrada() : "Nunca entrou");
+
+            return dados;
+        }).toList();
+
+        return ResponseEntity.ok(resultado);
+    }
 }
+
--- src/main/java/com/patrick/Estacionamento/repositories/VeiculoRepository.java
+++ src/main/java/com/patrick/Estacionamento/repositories/VeiculoRepository.java
@@ -1,6 +1,7 @@
 package com.patrick.Estacionamento.repositories;
 
 import com.patrick.Estacionamento.entities.Veiculo;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Repository;
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
 
         Veiculo findByPlate(String placa);
-    }
+
+    Veiculo findByPlaca(@NotBlank(message = "Obrigatório informar a placa") String placa);
+}
 
 
 
