a5bd4e909f64c915295d3fae3215b9f4b531ed71
Patrick Rodrigues <patrickdev6@hotmail.com>
2025-03-31 17:33:07 -0300

    add lógica de find by placa

--- src/main/java/com/patrick/Estacionamento/repositories/RegistroEntradaRepository.java
+++ src/main/java/com/patrick/Estacionamento/repositories/RegistroEntradaRepository.java
@@ -5,11 +5,14 @@ import com.patrick.Estacionamento.entities.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RegistroEntradaRepository extends JpaRepository<RegistroEntrada, Long> {
 
     RegistroEntrada findTopByVeiculoOrderByDataHoraEntradaDesc(Veiculo veiculo);
 
+    Optional<RegistroEntrada> findByVeiculoPlacaAndDataHoraSaidaIsNull(String placa);
 }
 
 
