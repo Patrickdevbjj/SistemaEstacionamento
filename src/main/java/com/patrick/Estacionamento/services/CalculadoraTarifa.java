1e4ef81a9809f9dd004567333989873aad912509
Patrick Rodrigues <patrickdev6@hotmail.com>
2025-04-01 14:29:13 -0300

    modificando logica de tarifa

--- src/main/java/com/patrick/Estacionamento/services/CalculadoraTarifa.java
+++ src/main/java/com/patrick/Estacionamento/services/CalculadoraTarifa.java
@@ -1,39 +1,33 @@
 package com.patrick.Estacionamento.services;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Service
+@Getter
+@AllArgsConstructor
 public class CalculadoraTarifa implements CalculadoraTarifaInterface {
+    // Valores padrão
+    private final double valorDiaria = 70.0;
+    private final double valorPorHora = 10.0;
+    private final int minutosTolerancia = 15;
+    private final int horasMinimasParaDiaria = 9;
 
-    @Value("${Estacionamento.tarifa.hora:9.0}")
-    private double valorPorHora;
 
-    @Value("${Estacionamento.tarifa.diaria:70.0")
-    private double valorDiaria;
 
-    @Value("${estacionamento.tolerancia.minutos:15}")
-    private int minutosTolerancia;
-
-    @Value("${estacionamento.diaria.horas-minimas:7}")
-    private int horasMinimasParaDiaria;
-
-    @Override
     public double calcular(LocalDateTime dataHoraEntrada, LocalDateTime dataHoraSaida) {
         long minutos = Duration.between(dataHoraEntrada, dataHoraSaida).toMinutes();
 
-        // Aplica tolerância gratuita
         if (minutos <= minutosTolerancia) {
             return 0.0;
         }
 
-        // Calcula horas arredondando para cima
-        long horas = (minutos + 59) / 60;  // Forma eficiente de arredondar para cima
+        long horas = (minutos + 59) / 60;
 
-        // Verifica se aplica tarifa diária
         if (horas >= horasMinimasParaDiaria) {
             return valorDiaria;
         }
